40ad7c18ae375a0db6803ef710946f24ed52c023
GianDutra <chikegang@gmail.com>
2023-10-07 17:38:40 -0300

    feat: implement communication between layers and presenters

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,37 +1,38 @@
-import { Controller, Get, Query, UseGuards } from "@nestjs/common";
-import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard";
-import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
-import { PrismaService } from "@/infra/database/prisma/prisma.service";
-import { z } from "zod";
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { QuestionPresenter } from '../presenters/question-presenter'
 
 const pageQueryParamSchema = z
   .string()
   .optional()
-  .default("1")
+  .default('1')
   .transform(Number)
-  .pipe(z.number().min(1));
+  .pipe(z.number().min(1))
 
-const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
 
-type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>;
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
 
-@Controller("/questions")
+@Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class FetchRecentQuestionsController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private fetchRecentQuestions: FetchRecentQuestionsUseCase) {}
 
   @Get()
-  async handle(@Query("page", queryValidationPipe) page: PageQueryParamSchema) {
-    const perPage = 20;
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
+    const result = await this.fetchRecentQuestions.execute({
+      page,
+    })
 
-    const questions = await this.prisma.question.findMany({
-      take: perPage,
-      skip: (page - 1) * perPage,
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
+    if (result.isLeft()) {
+      throw new Error()
+    }
 
-    return { questions };
+    const questions = result.value.questions
+
+    return { questions: questions.map(QuestionPresenter.toHTTP) }
   }
 }
